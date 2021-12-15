c59751252e5f6a66bf879198a133c970a5159242
ismail-fdilat <isfdilat@gmail.com>
2021-12-15 09:07:50 +0100

    first commit setting up the structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import Section1 from "./components/Section1";
 
+import Section2 from "./components/Section2";
+
+import Section3 from "./components/Section3";
+
+import Section4 from "./components/Section4";
+
+import Section5 from "./components/Section5";
+import Section6 from "./components/Section6";
+import Section7 from "./components/Section7";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar></Navbar>
+      <Section1></Section1>
+
+      <Section2></Section2>
+
+      <Section3></Section3>
+
+      <Section4></Section4>
+
+      <Section5></Section5>
+      <Section6></Section6>
+      <Section7></Section7>
+      <Contact></Contact>
+      <Footer></Footer>
     </div>
   );
 }
